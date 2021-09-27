dd8d3a2ee4aaa70cbd6c60589814f1f3b51b0f72
TheCrispySavage <91352420+TheCrispySavage@users.noreply.github.com>
2021-09-26 22:55:31 -0400

    Movement Fix

--- Fight for the Fifth Dynasty/Assets/Scripts/MoveTest.cs
+++ Fight for the Fifth Dynasty/Assets/Scripts/MoveTest.cs
@@ -13,7 +13,7 @@ public class MoveTest : MonoBehaviour
 
     public float defaultWalkDelay = 0.2f;
     public float walkDelay = 0.2f;
-    public int u = 1;
+    public int u = 3;
     public float walkDistance = 0.5f;
 
     Vector2 directionFacing;
@@ -31,29 +31,37 @@ public class MoveTest : MonoBehaviour
 
         if (walkDelay <= 0)
         {
-            if (u == 1)
+            if (u == 3)
             {
                 spriteRenderer.sprite = facingUp;
                 directionFacing = Vector2.up * walkDistance;
                 TryMoveInDirectionFacing();
 
+                u = 2;
+            }
+            else if (u == 2)
+            {
                 spriteRenderer.sprite = facingUp;
                 directionFacing = Vector2.up * walkDistance;
+                TryMoveInDirectionFacing();
+
+                u = 1;
+            }
+            else if (u == 1)
+            {
+                spriteRenderer.sprite = facingDown;
+                directionFacing = Vector2.down * walkDistance;
                 TryMoveInDirectionFacing();
 
                 u = 0;
             }
-            else if (u != 1)
+            else if (u == 0)
             {
                 spriteRenderer.sprite = facingDown;
                 directionFacing = Vector2.down * walkDistance;
                 TryMoveInDirectionFacing();
 
-                spriteRenderer.sprite = facingDown;
-                directionFacing = Vector2.down * walkDistance;
-                TryMoveInDirectionFacing();
-
-                u = 1;
+                u = 3;
             }
         }
         void TryMoveInDirectionFacing()
